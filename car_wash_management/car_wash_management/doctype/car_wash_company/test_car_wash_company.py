b155334bf5f942fa2e8826ea0edd123c021ac5e0
Rifat Dzhumagulov <rdzhumagulov@samokat.ru>
2024-08-11 16:32:38 +0300

    feat(mobile-api): Добавлен мобильный gateway

---
+++ car_wash_management/car_wash_management/doctype/car_wash_company/test_car_wash_company.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Rifat and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCarwashcompany(FrappeTestCase):
+	pass
