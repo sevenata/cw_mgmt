2f885cb43772626f411062e17d84d5510c699619
Rifat Dzhumagulov <rdzhumagulov@samokat.ru>
2024-08-13 02:22:22 +0300

    Экспорт fixtures (workflow, workflow action, workflow state)
    bench --site yourSite export-fixtures

--- car_wash_management/hooks.py
+++ car_wash_management/hooks.py
@@ -227,3 +227,13 @@ app_license = "mit"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+fixtures = [{
+	"doctype": "Workflow"
+},
+	{
+		"doctype": "Workflow State"
+	},
+	{
+		"doctype": "Workflow Action"
+	}
+]
