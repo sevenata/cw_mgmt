aaff2b6f779950014e5dda6e2c5741c88fe31f90
Rifat Dzhumagulov <rdzhumagulov@samokat.ru>
2025-04-21 19:55:14 +0300

    Добавлено поле staff_reward в запрос цен на услуги и обновлены настройки GitSharedSettings

--- car_wash_management/car_wash_management/doctype/car_wash_service/car_wash_service.py
+++ car_wash_management/car_wash_management/doctype/car_wash_service/car_wash_service.py
@@ -37,7 +37,7 @@ def get_services_with_prices():
         # Fetch related prices from Car wash service price
         prices = frappe.get_all(
             "Car wash service price",
-            fields=["price", "body_type", "name"],
+            fields=["price", "body_type", "name", "staff_reward"],
             filters={"base_service": service["name"]}
         )
         # Add prices field to each service
