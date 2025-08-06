359e08dc70c6295e024332699b0e5c5d6479600a
Rifat Dzhumagulov <rdzhumagulov@samokat.ru>
2025-08-06 18:14:58 +0300

    Добавлены новые типы

---
+++ car_wash_management/car_wash_management/doctype/car_wash_appointment_consumable_usage/test_car_wash_appointment_consumable_usage.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Rifat and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCarwashappointmentconsumableusage(FrappeTestCase):
+	pass
---
+++ car_wash_management/car_wash_management/doctype/mobile_app_user_saved_card/mobile_app_user_saved_card.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Rifat and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Mobile App User Saved Card", {
+// 	refresh(frm) {
+
+// 	},
+// });
