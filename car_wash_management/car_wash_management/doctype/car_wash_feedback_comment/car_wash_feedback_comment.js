c9c7e5ea9791cd601c2a4caee7ffab2921ae5dab
Rifat Dzhumagulov <rdzhumagulov@samokat.ru>
2025-08-17 18:32:59 +0300

    Добавлен тип Car wash feedback comment

---
+++ car_wash_management/car_wash_management/doctype/car_wash_feedback_comment/car_wash_feedback_comment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Rifat and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Car wash feedback comment", {
+// 	refresh(frm) {
+
+// 	},
+// });
